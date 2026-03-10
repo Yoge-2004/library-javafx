9338e9debd0f0e014b192d9c1a44c8b6c0fee83e
Yoge-2004 <yoge852004@gmail.com>
2026-03-10 14:00:41 +0530

    Store Windows app image via Git LFS

--- target/installer/LibraryApp/app/LibraryApp/bin/LibraryApp.bat
+++ target/installer/LibraryApp/app/LibraryApp/bin/LibraryApp.bat
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:925fb18a0621e8d7698e41df56b32450b024c67ab3f318a7cfb702cd3a1c471c
-size 181
+oid sha256:747ea500ffef8cfbac8d815311e069bcbec59dd3f5e0548cf7bbe36014b87721
+size 185
